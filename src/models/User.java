f8edddd8323e8f93e545120c6919d12ff0a195d8
oktay-osman <oktai.osman21@gmail.com>
2023-10-18 23:59:02 +0300

    added model class 'User'

---
+++ src/models/User.java
@@ -0,0 +1,27 @@
+package src.models;
+
+public class User {
+    public String name;
+    public String address;
+
+    public User(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
